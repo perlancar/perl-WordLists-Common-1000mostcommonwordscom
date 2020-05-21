a0be8bc0fe0f42f812561578a006f55ae27f604d
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-05-21 20:31:49 +0700

    Initial commit

---
+++ lib/WordLists/Common/1000mostcommonwordscom.pm
@@ -0,0 +1,22 @@
+package WordLists::Common::1000mostcommonwordscom;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Various lists of common words from 1000mostcommonwords.com
+
+=head1 DESCRIPTION
+
+Mostly for testing only. Do not use for any real purpose. The lists are garbage;
+the non-English list is a translation from the English one. Even the English
+list is suspect.
+
+
+=head1 SEE ALSO
+
+L<WordLists::ID::Common>
+
+=cut
